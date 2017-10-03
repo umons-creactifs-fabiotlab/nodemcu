7b023bc28c31cf442a9ca9b38e55385e4a36d1e3
François Roland <francois.roland@umons.ac.be>
2017-10-03 16:39:46 +0200

    Write web server code to handle changes to servo position.

--- WiFiWebServer/WiFiWebServer.ino
+++ WiFiWebServer/WiFiWebServer.ino
@@ -1,25 +1,56 @@
 #include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
 #include <Servo.h>
 
-const char* ssid = "froland-xps15-l";
-const char* password = "Bv3IwsJi";
+const char* ssid = "xxx";
+const char* password = "xxx";
 
 Servo myservo;
 int pos;
 
-// Create an instance of the server
-// specify the port to listen on as an argument
-WiFiServer server(80);
+ESP8266WebServer server(80);
+
+/* Handle the GET / request. */
+void handleStatus() {
+  // Prepare the response
+  String s = "<!DOCTYPE HTML>\r\n<html><p>Servo position is now: " + String(pos) + "°</p>";
+  s += "<form method=\"POST\" action=\"/\">";
+  s += "<p><input type=\"radio\" name=\"pos\" value=\"0\"";
+  if (pos == 0) s += " checked";
+  s += "> 0°</p>";
+  s += "<p><input type=\"radio\" name=\"pos\" value=\"90\"";
+  if (pos == 90) s += " checked";
+  s += "> 90°</p>";
+  s += "<p><input type=\"radio\" name=\"pos\" value=\"180\"";
+  if (pos == 180) s += " checked";
+  s += "> 180°</p>";
+  s += "<p><input type=\"submit\"></p>";
+  s += "</form>";
+  s += "</html>\n";
+  server.send(200, "text/html", s);
+}
+
+/* Handle requests to change position. */
+void handleChangePosition() {
+  pos = server.arg("pos").toInt();
+  int realPos = map(pos, 0, 180, 0, 160); /* our servo doesn't accept more than 160 */
+  myservo.attach(D1);
+  myservo.write(realPos);
+  handleNotFound();
+  delay(1000);
+  myservo.detach();
+}
+
+/* Handle all the other requests. */
+void handleNotFound() {
+  server.sendHeader("Location", "/", true);
+  server.send(303, "text/plain", "");
+}
 
 void setup() {
   Serial.begin(115200);
   delay(10);
 
-  // prepare servo
-  myservo.attach(D1);
-  myservo.write(0);
-  pos = 0;
-  
   // Connect to WiFi network
   Serial.println();
   Serial.println();
@@ -27,6 +58,13 @@ void setup() {
   Serial.println(ssid);
   
   WiFi.begin(ssid, password);
+  
+  // prepare servo
+  myservo.attach(D1);
+  pos = 0;
+  myservo.write(0);
+  delay(1000);
+  myservo.detach();
   
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
@@ -36,6 +74,9 @@ void setup() {
   Serial.println("WiFi connected");
   
   // Start the server
+  server.on("/", HTTP_GET, handleStatus);
+  server.on("/", HTTP_POST, handleChangePosition);
+  server.onNotFound(handleNotFound);
   server.begin();
   Serial.println("Server started");
 
@@ -44,65 +85,6 @@ void setup() {
 }
 
 void loop() {
-  // Check if a client has connected
-  WiFiClient client = server.available();
-  if (!client) {
-    return;
-  }
-  
-  // Wait until the client sends some data
-  Serial.println("new client");
-  while(!client.available()){
-    delay(1);
-  }
-  
-  // Read the first line of the request
-  String req = client.readStringUntil('\r');
-  Serial.println(req);
-  client.flush();
-  
-  // Match the request
-  int val;
-  if (req.indexOf("GET") != -1) {
-    client.flush();
-  
-    // Prepare the response
-    String s = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<!DOCTYPE HTML>\r\n<html><p>Servo position is now: " + String(pos) + "°</p>";
-    s += "<form method=\"POST\" action=\"/\">";
-    s += "<p><input type=\"text\" name=\"pos\"> <input type=\"submit\"></p>";
-    s += "</form>";
-    s += "</html>\n";
-  
-    // Send the response to the client
-    client.print(s);
-  } else {
-    if (req.indexOf("POST") != -1) {
-      String formData;
-      int retry = 0;
-      do {
-        req = client.readStringUntil('\r');
-        client.readStringUntil('\n');
-        if (req.indexOf("pos=") != -1) {
-          int index = req.indexOf("pos=") + 4;
-          pos = req.substring(index).toInt();
-          myservo.write(pos);
-        }
-        if (req == "") break;
-        retry++;
-      } while(req.length() == 0 && retry < 3);
-    }
-    client.flush();
-
-    String s = "HTTP/1.1 303 See Other\r\n";
-    s +="Location: /\r\n";
-
-    client.print(s);
-  }
-
-  delay(1);
-  Serial.println("Client disonnected");
-
-  // The client will actually be disconnected 
-  // when the function returns and 'client' object is detroyed
+  server.handleClient();
 }
 

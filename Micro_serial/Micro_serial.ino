3caffdc838b652575c40e18f77f2c61790345d8e
hereticmaggot <aurelien.vanlaere@gmail.com>
2017-10-19 10:12:45 +0200

    Create Micro_serial.ino

---
+++ Micro_serial/Micro_serial.ino
@@ -0,0 +1,33 @@
+#include <Servo.h>
+
+Servo servo; //Definition of an instance of servo
+String var;
+
+void setup() 
+{
+  Serial.begin(115200);
+  delay(10);
+  Serial.setTimeout(1000); //MAX TIMEOUT
+
+  servo.attach(D1);
+  servo.write(0);
+  Serial.print("Program began\n");
+}
+
+void loop() 
+{
+  Serial.print("Waiting for command:\n");
+  do
+  {
+    var=Serial.readStringUntil('\n');
+  }while(var.length()==0);
+  
+  Serial.print("\nReceived command from serial port: ");
+  Serial.print(var);
+  Serial.print("\n\n");
+  servo.write(var.toInt()%181);
+  Serial.print("Servo moved to ");
+  Serial.print(var.toInt()%181);
+  Serial.print(" deg\n\n");
+
+}
